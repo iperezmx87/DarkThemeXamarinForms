4f023286dcfd915201f290fd6bee2d81115d1af7
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2020-06-11 20:10:34 -0500

    Add files via upload

---
+++ DemoColores/DemoColores/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoColores
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // agregar una etiqueta dinamica
+            Label lblSaludo = new Label()
+            {
+                Text = "Hola, soy claro y oscuro",
+                FontSize = 24,
+                TextColor = ((AppThemeColor)App.Current.Resources["frameTextColor"]).Value
+            };
+
+            fraContenedor.Content = lblSaludo;
+        }
+    }
+}
